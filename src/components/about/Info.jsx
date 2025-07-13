4d37b093b7a8e09d37f72287f56d690a34fa8908
João Beserra <joao.beserra@aluno.ifsp.edu.br>
2025-07-13 18:19:54 -0300

    feat: About section finalized; initiated responsive Skills section

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Info = () => {
+    return (
+        <div className="about__info grid" >
+            <div className="about__box">
+                <i class='bxr  bx-medal-star-alt about__icon'  ></i> 
+                <h3 className="about__title">
+                    Experiência
+                </h3>
+                <span className="about__subtitle">
+                    2y+ comendo DevWeb
+                </span>
+            </div>
+            <div className="about__box">
+                <i class='bxr  bx-briefcase-alt-2 about__icon'  ></i> 
+                <h3 className="about__title">
+                    Workshop
+                </h3>
+                <span className="about__subtitle">
+                    + 10 projetos desenvolvidos 
+                </span>
+            </div>
+            <div className="about__box">
+                <i class='bxr  bx-certification about__icon'  ></i> 
+                <h3 className="about__title">
+
+                    Habilidades
+                </h3>
+                <span className="about__subtitle">
+                    ReactJS, Python Java e +
+                </span>
+            </div>
+
+        </div>
+    )
+}
+
+export default Info;
